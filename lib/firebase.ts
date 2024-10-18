e0b5e88e76f7fb534cd6f0b02bf96c1154a5e03e
ccheshirecat <spuncheshirecat@gmail.com>
2024-10-19 06:38:36 +0700

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-}
+  apiKey: "AIzaSyBXAzbrS81o2xzuby_mKy1T5fQFYfApVq8",
+  authDomain: "porterplays-e0c1d.firebaseapp.com",
+  projectId: "porterplays-e0c1d",
+  storageBucket: "porterplays-e0c1d.appspot.com",
+  messagingSenderId: "494457827029",
+  appId: "1:494457827029:web:4df3f3ff1c7cf43647bc5c",
+  measurementId: "G-GMW2B0DRBS"
+};
 
 // Initialize Firebase if it hasn't been initialized yet
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
