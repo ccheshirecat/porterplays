081137d432d79b1771fa2f08e08808ae542eb896
CheshireCat <spuncheshirecat@gmail.com>
2024-10-23 21:15:31 +0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { NavMenu } from "@/components/NavMenu";
 import Footer from "@/components/footer";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
 import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
@@ -22,7 +15,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
     <html lang="en">
       <body className="bg-background text-foreground font-sans">
         <NavMenu />
@@ -32,6 +24,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-    </ClerkProvider>
   );
 }
